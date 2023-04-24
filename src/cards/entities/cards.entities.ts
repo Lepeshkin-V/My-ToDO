5545cd8effb64b1ab0f8f75e709a9e310ae2e443
7thDragon <slava.lepeshkin01@gmail.com>
2023-04-24 23:53:44 +0600

    feat: add struct cards

---
+++ src/cards/entities/cards.entities.ts
@@ -0,0 +1,21 @@
+import { type } from "os";
+import { Note } from "src/notes/entities/notes.entity";
+import { Column, CreateDateColumn, Entity, ObjectIdColumn } from "typeorm";
+
+@Entity()
+export class Card {
+    @ObjectIdColumn()
+    id: number;
+
+    @Column()
+    date: Date;
+
+    @Column((type) => Note)
+    notes: Note[];
+
+    @CreateDateColumn({type: 'timestamp'})
+    createdAt: Date;
+
+    @CreateDateColumn({type: 'timestamp'})
+    updatedAt: Date;
+}
