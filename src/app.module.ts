57117bc4dbae6151686043898dcc46f0fb57e92e
7thDragon <slava.lepeshkin01@gmail.com>
2023-05-16 01:41:23 +0600

    feat: add db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,16 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Note } from "./notes/entities/notes.entity";
 import { Card } from "./cards/entities/cards.entities";
+import { Table } from "./tables/entities/tables.entity";
+import { User } from "./users/entities/users.entity";
+import {DB_URL} from './config';
 
 @Module({
     imports: [TypeOrmModule.forRoot({
         type: 'mongodb',
-        entities: [Note, Card],
+        url: DB_URL,
+        database: "ToDo",
+        entities: [Note, Card, Table, User],
     })],
 })
 
