62767f2778cdb6c623e36c7fdb46fbc1a63b59ec
7thDragon <slava.lepeshkin01@gmail.com>
2023-05-18 00:57:43 +0600

    fix: remove cards

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,19 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Note } from "./notes/entities/notes.entity";
-import { Card } from "./cards/entities/cards.entities";
 import { Table } from "./tables/entities/tables.entity";
 import { User } from "./users/entities/users.entity";
 import {DB_URL} from './config';
+import { NoteModule } from "./notes/notes.module";
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
+    imports: [
+        NoteModule,
+        TypeOrmModule.forRoot({
         type: 'mongodb',
         url: DB_URL,
         database: "ToDo",
-        entities: [Note, Card, Table, User],
+        entities: [Note, Table, User],
     })],
 })
 
