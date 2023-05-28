c8ee447a1fa781a442e18385dfb57367b6667f14
7thDragon <slava.lepeshkin01@gmail.com>
2023-05-28 19:31:13 +0600

    feat: add order

--- src/notes/services/notes.service.ts
+++ src/notes/services/notes.service.ts
@@ -6,7 +6,21 @@ import { CreateNoteDto } from '../dtos/create-note.dto';
 import { UpdateNoteDto } from '../dtos/update-note.dto';
 import { ObjectId } from 'mongodb';
 import { GetForDateDto } from '../dtos/get-for-date.dto';
-
+export function timer() {
+  let timeStart = new Date().getTime();
+  return {
+      /** <integer>s e.g 2s etc. */
+      get seconds() {
+          const seconds = Math.ceil((new Date().getTime() - timeStart) / 1000) + 's';
+          return seconds;
+      },
+      /** Milliseconds e.g. 2000ms etc. */
+      get ms() {
+          const ms = (new Date().getTime() - timeStart) + 'ms';
+          return ms;
+      }
+  }
+}
 @Injectable()
 export class NotesService {
   constructor(
@@ -37,6 +51,7 @@ export class NotesService {
   }
 
   async findForWeek(getWeekDto: GetForDateDto): Promise<Note[]> {
+    const howLong = timer();
     const dates: string[] = [];
     const currentDate = new Date(getWeekDto.date);
     for (let i = currentDate.getDate(); i < currentDate.getDate() + 7; i++) {
@@ -44,8 +59,12 @@ export class NotesService {
       d.setDate(i);
       dates.push(d.toLocaleDateString());
     }
+    console.log(howLong.ms)
     return this.notesRepository.find({
       where: { tableId: getWeekDto.tableId, date: { $in: dates } },
+      order:{
+        date: "ASC"
+      }
     });
   }
 
