602a447371acc306fbf4003f229a75ae321c9c31
7thDragon <slava.lepeshkin01@gmail.com>
2023-06-06 22:50:13 +0600

    feat: add order

--- src/notes/services/notes.service.ts
+++ src/notes/services/notes.service.ts
@@ -47,6 +47,9 @@ export class NotesService {
     }
     return this.notesRepository.find({
       where: { tableId: getWeekDto.tableId, date: { $in: dates } },
+      order:{
+        date: "ASC"
+      }
     });
   }
 
